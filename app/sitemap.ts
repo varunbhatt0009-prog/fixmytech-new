b7e8353bf7926c04c2b3264ca1496778f4dc1813
Varun Bhatt <varunbhatt8080@gmail.com>
2026-04-16 18:09:30 +0530

    fix sitemap for nextjs metadata route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,44 +1,22 @@
+import type { MetadataRoute } from "next";
 import { getAllPosts } from "@/lib/posts";
 import { siteConfig } from "@/lib/site";
 
-export async function GET() {
+export default function sitemap(): MetadataRoute.Sitemap {
   const posts = getAllPosts();
 
-  const urls = [
-    `
-    <url>
-      <loc>${siteConfig.url}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>1.0</priority>
-    </url>
-    `,
-    `
-    <url>
-      <loc>${siteConfig.url}/blog</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.9</priority>
-    </url>
-    `,
-    ...posts.map(
-      (post) => `
-      <url>
-        <loc>${siteConfig.url}/blog/${post.slug}</loc>
-        <lastmod>${post.date}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-      </url>
-    `
-    )
-  ].join("");
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
-  </urlset>`;
-
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
+  return [
+    {
+      url: siteConfig.url,
+      lastModified: new Date(),
     },
-  });
+    {
+      url: `${siteConfig.url}/blog`,
+      lastModified: new Date(),
+    },
+    ...posts.map((post) => ({
+      url: `${siteConfig.url}/blog/${post.slug}`,
+      lastModified: new Date(post.date),
+    })),
+  ];
 }
